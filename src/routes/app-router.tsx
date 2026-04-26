f8c36e418775c4cd9e12b71d417895df3247baff
jhosep98 <jhosep149@hotmail.com>
2026-04-26 15:46:58 -0300

    fix: Router provider with v7_startTransition

--- src/routes/app-router.tsx
+++ src/routes/app-router.tsx
@@ -3,7 +3,14 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 
 const AppRouter: React.FC = () => {
-  return <RouterProvider router={router} />
+  return (
+    <RouterProvider
+      router={router}
+      future={{
+        v7_startTransition: true,
+      }}
+    />
+  )
 }
 
 export default AppRouter
